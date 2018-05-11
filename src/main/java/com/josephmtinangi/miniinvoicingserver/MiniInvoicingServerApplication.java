74e38c3109ad91f2ef863bee999b9b65d72fd429
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-11 09:34:12 +0300

    Initial commit

---
+++ src/main/java/com/josephmtinangi/miniinvoicingserver/MiniInvoicingServerApplication.java
@@ -0,0 +1,12 @@
+package com.josephmtinangi.miniinvoicingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniInvoicingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniInvoicingServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/josephmtinangi/miniinvoicingserver/MiniInvoicingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.josephmtinangi.miniinvoicingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiniInvoicingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
